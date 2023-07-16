97e7e49c4c60913b40392e98081826e9b7f1c4cc
gloofo <gloofo@gloofo.glof>
2023-07-16 12:23:36 +0800

    Removed unecessary modules

--- src/modules.py
+++ src/modules.py
@@ -3,8 +3,5 @@ import yaml
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from time import sleep
